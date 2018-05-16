927656e39c93784017c574fa6efcae4b8062b3b0
Sanjoli99 <39343308+Sanjoli99@users.noreply.github.com>
2018-05-16 21:59:40 +0530

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int floorSqrt(int x)
+{
+	if(x==0 || x==1)
+	{
+		return x;
+	}
+	int start=1, end=x, ans;
+	while(start <= end)
+	{
+		if(mid*mid == x)
+		return mid;
+		
+		if(mid*mid < x)
+		{
+			
+		}
+	}
+}
